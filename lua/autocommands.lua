5e71356fd15722c7d2a88b94b8f83d7b134d0228
bryant-the-coder <bryantfoo99@gmail.com>
2022-01-01 16:54:54 +0800

    change the highlight for nvim-cmp

--- lua/autocommands.lua
+++ lua/autocommands.lua
@@ -38,14 +38,19 @@ cmd([[
 
 -- This is for nvim-cmp
 cmd([[
+  " gray
   highlight! CmpItemAbbrDeprecated guibg=NONE gui=strikethrough guifg=#808080
+  " blue
   highlight! CmpItemAbbrMatch guibg=NONE guifg=#569CD6
   highlight! CmpItemAbbrMatchFuzzy guibg=NONE guifg=#569CD6
+  " light blue
   highlight! CmpItemKindVariable guibg=NONE guifg=#9CDCFE
   highlight! CmpItemKindInterface guibg=NONE guifg=#9CDCFE
   highlight! CmpItemKindText guibg=NONE guifg=#9CDCFE
+  " pink
   highlight! CmpItemKindFunction guibg=NONE guifg=#C586C0
   highlight! CmpItemKindMethod guibg=NONE guifg=#C586C0
+  " front
   highlight! CmpItemKindKeyword guibg=NONE guifg=#D4D4D4
   highlight! CmpItemKindProperty guibg=NONE guifg=#D4D4D4
   highlight! CmpItemKindUnit guibg=NONE guifg=#D4D4D4
