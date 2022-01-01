10c104bfd8ddbeb95941b8c6f41f37c1acce9b96
bryant-the-coder <bryantfoo99@gmail.com>
2022-01-01 16:58:26 +0800

    changed the icons for gitsigns :))
    REASON:
    1. They are goddamn hard to read
    2. Better visualization

--- lua/plugins/other.lua
+++ lua/plugins/other.lua
@@ -39,7 +39,6 @@ require("indent_blankline").setup {
     },
 }
 
--- Git
 require('gitsigns').setup {
   current_line_blame_opts = {
     virt_text = true,
@@ -47,5 +46,12 @@ require('gitsigns').setup {
     delay = 1000,
     ignore_whitespace = false,
   },
-  current_line_blame = true
+  current_line_blame = true,
+  signs = {
+    add          = {hl = 'GitSignsAdd'   , text = '│', numhl='GitSignsAddNr'   , linehl='GitSignsAddLn'},
+    change       = {hl = 'GitSignsChange', text = '│', numhl='GitSignsChangeNr', linehl='GitSignsChangeLn'},
+    delete       = {hl = 'GitSignsDelete', text = '-', numhl='GitSignsDeleteNr', linehl='GitSignsDeleteLn'},
+    topdelete    = {hl = 'GitSignsDelete', text = '-', numhl='GitSignsDeleteNr', linehl='GitSignsDeleteLn'},
+    changedelete = {hl = 'GitSignsChange', text = '-', numhl='GitSignsChangeNr', linehl='GitSignsChangeLn'},
+  },
 }
