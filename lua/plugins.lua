0c67f9a80fe5bece6d058c6fa476e64461f286c9
bryant-the-coder <bryantfoo99@gmail.com>
2022-01-05 10:21:28 +0800

    read by yourself :))
    1. Added function?
    2. Lazy load (fun)
    3. Remove & added plugins including null

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,140 +1,173 @@
 local fn = vim.fn
-local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
 if fn.empty(fn.glob(install_path)) > 0 then
-  packer_bootstrap = fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+	packer_bootstrap = fn.system({
+		"git",
+		"clone",
+		"--depth",
+		"1",
+		"https://github.com/wbthomason/packer.nvim",
+		install_path,
+	})
 end
 
-return require('packer').startup(function(use)
-    -- Packer can manage itself
-    use 'wbthomason/packer.nvim'
+return require("packer").startup(function(use)
+	-- Packer can manage itself
+	use("wbthomason/packer.nvim")
 
-    -- Dependencies
-    use 'nvim-lua/plenary.nvim'
-    use 'kyazdani42/nvim-web-devicons'
+	-- Dependencies
+	use("nvim-lua/plenary.nvim")
+	use("kyazdani42/nvim-web-devicons")
 
-    -- Theme
-    use 'folke/tokyonight.nvim'
-    use 'morhetz/gruvbox'
-    use {'rose-pine/neovim', as = "rose-pine"}
+	-- Theme
+	use("folke/tokyonight.nvim")
+	use("morhetz/gruvbox")
+	use({ "rose-pine/neovim", as = "rose-pine" })
 
-    -- Explorer menu
-    use {'kyazdani42/nvim-tree.lua', config = [[require('plugins.nvim-tree')]]}
+	-- Explorer menu
+	use({ "kyazdani42/nvim-tree.lua", config = [[require('plugins.nvim-tree')]] })
 
-    -- Treesitter
-    use {
-        'nvim-treesitter/nvim-treesitter',
-        run = ":TSUpdate",
-        requires = {
-         'p00f/nvim-ts-rainbow',
-         'nvim-treesitter/playground',
-         'windwp/nvim-ts-autotag',
-         {'windwp/nvim-autopairs', config = [[require('plugins.autopairs')]]},
-      },
-      config = [[require('plugins.treesitter')]]
-    }
+	-- Treesitter
+	use({
+		"nvim-treesitter/nvim-treesitter",
+		run = ":TSUpdate",
+		requires = {
+			"p00f/nvim-ts-rainbow",
+			"nvim-treesitter/playground",
+			"windwp/nvim-ts-autotag",
+			{ "windwp/nvim-autopairs", config = [[require('plugins.autopairs')]] },
+		},
+		config = [[require('plugins.treesitter')]],
+	})
 
-    -- Statusline
-    use {'nvim-lualine/lualine.nvim', config = [[require('plugins.lualine')]]}
+	-- Statusline
+	use({ "nvim-lualine/lualine.nvim", config = [[require('plugins.lualine')]] })
 
-    -- Telescope
-    use {
-      'nvim-telescope/telescope.nvim',
-      cmd = 'Telescope',
-      module = 'telescope',
-      config = [[require('plugins.telescope')]],
-    }
+	-- Telescope
+	use({
+		"nvim-telescope/telescope.nvim",
+		cmd = "Telescope",
+		module = "telescope",
+		config = [[require('plugins.telescope')]],
+	})
 
-    -- Dashboard
-    use {'glepnir/dashboard-nvim', config = [[require('plugins.dashboard')]]}
+	-- Dashboard
+	use({ "glepnir/dashboard-nvim", config = [[require('plugins.dashboard')]] })
 
-    -- Bufferline
-    use {'akinsho/bufferline.nvim', config = [[require('plugins.bufferline')]]}
+	-- Bufferline
+	use({ "akinsho/bufferline.nvim", config = [[require('plugins.bufferline')]] })
 
-    -- Colorizer
-    use {
-      'norcalli/nvim-colorizer.lua',
-      config = [[require('plugins.other')]]
-    }
+	-- Colorizer
+	use({
+		"norcalli/nvim-colorizer.lua",
+		config = [[require('plugins.other')]],
+	})
 
-    -- LSP
-    use {'neovim/nvim-lspconfig', config = [[require('plugins.lsp')]]}
-    use {
-      'hrsh7th/nvim-cmp',
-      requires = {
-        'hrsh7th/cmp-vsnip',
-        'hrsh7th/cmp-nvim-lsp',
-        'onsails/lspkind-nvim',
-        {'hrsh7th/vim-vsnip', after = 'nvim-cmp'},
-        {'hrsh7th/cmp-nvim-lsp', after = 'nvim-cmp'},
-        {'hrsh7th/cmp-buffer', after = 'nvim-cmp'},
-        {'hrsh7th/cmp-path', after = 'nvim-cmp'},
-        {'hrsh7th/cmp-path', after = 'nvim-cmp'},
-        {'David-Kunz/cmp-npm', after = 'nvim-cmp'}
-      },
-      config = [[require('plugins.cmp')]]
-    }
+	-- LSP
+	use({ "neovim/nvim-lspconfig", config = [[require('plugins.lsp')]] })
+	use({
+		"hrsh7th/nvim-cmp",
+		requires = {
+			"hrsh7th/cmp-vsnip",
+			"hrsh7th/cmp-nvim-lsp",
+			"onsails/lspkind-nvim",
+			{ "hrsh7th/vim-vsnip", after = "nvim-cmp" },
+			{ "hrsh7th/cmp-nvim-lsp", after = "nvim-cmp" },
+			{ "hrsh7th/cmp-buffer", after = "nvim-cmp" },
+			{ "hrsh7th/cmp-path", after = "nvim-cmp" },
+			{ "hrsh7th/cmp-path", after = "nvim-cmp" },
+			{ "David-Kunz/cmp-npm", after = "nvim-cmp" },
+		},
+		config = [[require('plugins.cmp')]],
+	})
 
-    -- LSP highlight
-    use {'folke/lsp-colors.nvim', config = [[require('plugins.other')]]}
+	use({
+		"jose-elias-alvarez/null-ls.nvim",
+		config = [[require('plugins.null-ls')]],
+	})
 
-    -- Indentation
-    use {
-      'lukas-reineke/indent-blankline.nvim',
-      config = [[require('plugins.other')]]
-    }
+	-- LSP highlight
+	use({ "folke/lsp-colors.nvim", config = [[require('plugins.other')]] })
 
-    -- Auto-comment
-    use {
-      'numToStr/Comment.nvim',
-      requires = {
-        'JoosepAlviste/nvim-ts-context-commentstring'
-      },
-      config = [[require('plugins.comment')]]
-    }
+	-- Indentation
+	use({
+		"lukas-reineke/indent-blankline.nvim",
+		config = [[require('plugins.other')]],
+	})
 
-    -- Terminal
-    use {'akinsho/toggleterm.nvim', config = [[require('plugins.toggleterm')]]}
+	-- Auto-comment
+	use({
+		"numToStr/Comment.nvim",
+		requires = {
+			"JoosepAlviste/nvim-ts-context-commentstring",
+		},
+		config = [[require('plugins.comment')]],
+	})
 
-    -- Harpoon
-    use 'ThePrimeagen/harpoon'
+	-- Terminal
+	use({ "akinsho/toggleterm.nvim", config = [[require('plugins.toggleterm')]] })
 
-    -- Disturbance free writing
-    use {
-      'folke/twilight.nvim',
-      config = [[require('plugins.twilight')]]
-    }
+	-- Harpoon
+	use("ThePrimeagen/harpoon")
 
-    use {
-      'folke/zen-mode.nvim',
-      config = [[require('plugins.zen-mode')]]
-    }
-    -- Git
-    use {
-      'lewis6991/gitsigns.nvim',
-      config = function ()
-        require('gitsigns').setup {
-          current_line_blame_opts = {
-            virt_text = true,
-            virt_text_pos = 'eol', -- 'eol' | 'overlay' | 'right_align'
-            delay = 1000,
-            ignore_whitespace = false,
-          },
-          current_line_blame = true,
-          signs = {
-            add          = {hl = 'GitSignsAdd'   , text = '│', numhl='GitSignsAddNr'   , linehl='GitSignsAddLn'},
-            change       = {hl = 'GitSignsChange', text = '│', numhl='GitSignsChangeNr', linehl='GitSignsChangeLn'},
-            delete       = {hl = 'GitSignsDelete', text = '│', numhl='GitSignsDeleteNr', linehl='GitSignsDeleteLn'},
-            topdelete    = {hl = 'GitSignsDelete', text = '│', numhl='GitSignsDeleteNr', linehl='GitSignsDeleteLn'},
-            changedelete = {hl = 'GitSignsChange', text = '│', numhl='GitSignsChangeNr', linehl='GitSignsChangeLn'},
-          },
-          current_line_blame_formatter_opts = {
-            relative_time = true
-          },
-        }
-      end
-    }
+	-- Disturbance free writing
+	use({
+		"folke/twilight.nvim",
+		config = [[require('plugins.twilight')]],
+	})
 
-  if packer_bootstrap then
-    require('packer').sync()
-  end
+	use({
+		"folke/zen-mode.nvim",
+		config = [[require('plugins.zen-mode')]],
+	})
+
+	-- Git
+	use({
+		"lewis6991/gitsigns.nvim",
+		config = function()
+			require("gitsigns").setup({
+				current_line_blame_opts = {
+					virt_text = true,
+					virt_text_pos = "eol", -- 'eol' | 'overlay' | 'right_align'
+					delay = 1000,
+					ignore_whitespace = false,
+				},
+				current_line_blame = true,
+				signs = {
+					add = { hl = "GitSignsAdd", text = "│", numhl = "GitSignsAddNr", linehl = "GitSignsAddLn" },
+					change = {
+						hl = "GitSignsChange",
+						text = "│",
+						numhl = "GitSignsChangeNr",
+						linehl = "GitSignsChangeLn",
+					},
+					delete = {
+						hl = "GitSignsDelete",
+						text = "│",
+						numhl = "GitSignsDeleteNr",
+						linehl = "GitSignsDeleteLn",
+					},
+					topdelete = {
+						hl = "GitSignsDelete",
+						text = "│",
+						numhl = "GitSignsDeleteNr",
+						linehl = "GitSignsDeleteLn",
+					},
+					changedelete = {
+						hl = "GitSignsChange",
+						text = "│",
+						numhl = "GitSignsChangeNr",
+						linehl = "GitSignsChangeLn",
+					},
+				},
+				current_line_blame_formatter_opts = {
+					relative_time = true,
+				},
+			})
+		end,
+	})
+
+	if packer_bootstrap then
+		require("packer").sync()
+	end
 end)
