ec6bbcb6235173ab4ba7c02c14e1e02609191916
bryant-the-coder <bryantfoo99@gmail.com>
2022-01-09 12:50:25 +0800

    format it

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,111 +1,112 @@
 local function map(mode, lhs, rhs, opts)
-  local default_options = {noremap = true, silent = true}
-  if opts then default_options = vim.tbl_extend("force", default_options, opts) end
-  vim.api.nvim_set_keymap(mode, lhs, rhs, default_options)
+	local default_options = { noremap = true, silent = true }
+	if opts then
+		default_options = vim.tbl_extend("force", default_options, opts)
+	end
+	vim.api.nvim_set_keymap(mode, lhs, rhs, default_options)
 end
 
 -----------------------------------
 --           BASIC               --
 -----------------------------------
 -- Quitting
-map('n', '<C-q>', ':q<CR>')
+map("n", "<C-q>", ":q<CR>")
 
 -- Buffer
-map('n', '<leader>dd', ':bdelete<CR>')
+map("n", "<leader>dd", ":bdelete<CR>")
 
 -- ESC key
-map('i', 'jk', '<Esc>')
-map('i', 'JK', '<Esc>')
-map('v', 'jk', '<Esc>')
+map("i", "jk", "<Esc>")
+map("i", "JK", "<Esc>")
+map("v", "jk", "<Esc>")
 
 -- ESC to clear all highlights
-map('i', '<Esc>', ':noh<CR>')
-map('v', '<Esc>', ':noh<CR>')
-map('n', '<Esc>', ':noh<CR>')
+map("i", "<Esc>", ":noh<CR>")
+map("v", "<Esc>", ":noh<CR>")
+map("n", "<Esc>", ":noh<CR>")
 
 -- Terminal ESC key
-map('t', '<ESC>', [[<C-\><C-n>]])
+map("t", "<ESC>", [[<C-\><C-n>]])
 
 -- Don't yank text upon delete (good mapping btw)
-map('n', 'd', '"_d')
-map('v', 'd', '"_d')
+map("n", "d", '"_d')
+map("v", "d", '"_d')
 
 -- Don't yank text on cut
-map('n', 'x', '"_x')
-map('n', 'x', '"_x')
+map("n", "x", '"_x')
+map("n", "x", '"_x')
 
 -- Saving the traditional way
-map('n', '<C-s>', ':w<CR>')
-map('i', '<C-s>', ':w<CR>')
-map('n', '<leader>sf', ':source % <CR>')
+map("n", "<C-s>", ":w<CR>")
+map("i", "<C-s>", ":w<CR>")
+map("n", "<leader>sf", ":source % <CR>")
 map("n", "<leader>fs", ":lua vim.lsp.buf.formatting_sync(nil, 100)<CR>")
 
 -- Resizing windows
-map('n', '<A-up>', '<C-w>+')
-map('n', '<A-down>', '<C-w>-')
-map('n', '<A-left>', '<C-w>>')
-map('n', '<A-right>', '<C-w><')
+map("n", "<A-up>", "<C-w>+")
+map("n", "<A-down>", "<C-w>-")
+map("n", "<A-left>", "<C-w>>")
+map("n", "<A-right>", "<C-w><")
 
 -- Window Navigation
-map('n', 'J', '<C-w><C-j>')
-map('n', 'K', '<C-w><C-k>')
-map('n', 'L', '<C-w><C-l>')
-map('n', 'H', '<C-w><C-h>')
+map("n", "J", "<C-w><C-j>")
+map("n", "K", "<C-w><C-k>")
+map("n", "L", "<C-w><C-l>")
+map("n", "H", "<C-w><C-h>")
 
 -- Yanking mayb?
-map('v', '>', '>gv')
-map('v', '<', '<gv')
+map("v", ">", ">gv")
+map("v", "<", "<gv")
 
 -- Buffer navigation
-map('n', '<Tab>',   ':bnext<CR>')
-map('n', '<S-Tab>', ':bprevious<CR>')
-map('n', '<C-w>',   ':bdelete<CR>')
+map("n", "<Tab>", ":bnext<CR>")
+map("n", "<S-Tab>", ":bprevious<CR>")
+map("n", "<C-w>", ":bdelete<CR>")
 
 -- Moving lines up & down (complicated)
-map('n', '<C-down>', ':m .+1<CR>==')
-map('n', '<C-up>', ':m .-2<CR>==')
-map('v', '<C-down>', ":m '>+1<CR>gv=gv")
-map('v', '<C-up>', ":m '<-2<CR>gv=gv")
+map("n", "<C-down>", ":m .+1<CR>==")
+map("n", "<C-up>", ":m .-2<CR>==")
+map("v", "<C-down>", ":m '>+1<CR>gv=gv")
+map("v", "<C-up>", ":m '<-2<CR>gv=gv")
 
 -----------------------------------
 --           Plugins             --
 -----------------------------------
 -- Nvim-tree
-map('n', '<C-n>', ':NvimTreeToggle<CR>')
-
+map("n", "<C-n>", ":NvimTreeToggle<CR>")
 
 -- Zen-mode
-map('n', '<leader>zm', ':ZenMode<CR>')
+map("n", "<leader>zm", ":ZenMode<CR>")
 
 -- TSPlayground
-map('n', '<leader>tp', ':TSPlaygroundToggle<CR>')
-map('i', '<leader>tp', ':TSPlaygroundToggle<CR>')
+map("n", "<leader>tp", ":TSPlaygroundToggle<CR>")
+map("i", "<leader>tp", ":TSPlaygroundToggle<CR>")
 
--- Packer
-map('n', '<leader>pi', ':PackerInstall<CR>')
-map('n', '<leader>pu', ':PackerUpdate<CR>')
-map('n', '<leader>pc', ':PackerClean<CR>')
-map('n', '<leader>ps', ':PackerSync<CR>')
+-- Packe
+map("n", "<leader>pi", ":PackerInstall<CR>")
+map("n", "<leader>pu", ":PackerUpdate<CR>")
+map("n", "<leader>pc", ":PackerClean<CR>")
+map("n", "<leader>ps", ":PackerSync<CR>")
 
 -- LSP
-map('n', '<leader>lr', [[:lua vim.lsp.buf.rename()<CR>]])
-map('n', '<leader>ld', [[:lua vim.lsp.buf.definition()<CR>]])
-map('n', '<leader>lt', [[:lua vim.lsp.buf.type_definition()<CR>]])
-map('n', '<leader>lh', [[:lua vim.lsp.buf.signature_help()<CR>]])
-map('n', '<C-a>', [[:lua vim.lsp.buf.references()<CR>]])
-map('n', '<C-k>', [[:lua vim.diagnostic.goto_prev({border = "rounded"})<CR>]])
-map('n', '<C-j>', [[:lua vim.diagnostic.goto_next({border = "rounded"})<CR>]])
-map('n', '<leader>ss', '[[:lua vim.lsp.buf.formatting_sync()<CR>]]')
+map("n", "<leader>lr", [[:lua vim.lsp.buf.rename()<CR>]])
+map("n", "<leader>ld", [[:lua vim.lsp.buf.definition()<CR>]])
+map("n", "<leader>lt", [[:lua vim.lsp.buf.type_definition()<CR>]])
+map("n", "<leader>lh", [[:lua vim.lsp.buf.signature_help()<CR>]])
+map("n", "<C-a>", [[:lua vim.lsp.buf.references()<CR>]])
+map("n", "<C-k>", [[:lua vim.diagnostic.goto_prev({border = "rounded"})<CR>]])
+map("n", "<C-j>", [[:lua vim.diagnostic.goto_next({border = "rounded"})<CR>]])
+map("n", "<leader>ss", "[[:lua vim.lsp.buf.formatting_sync()<CR>]]")
 
 -- Harpooon
-map('n', '<A-p>', [[:lua require("harpoon.ui").toggle_quick_menu()<CR>]])
-map('n', '<leader>lf', [[:lua require("harpoon.mark").add_file()<CR>]])
-map('n', '<leader>l;', [[:lua require("harpoon.ui").nav_prev()<CR>]])
-map('n', '<leader>ll', [[:lua require("harpoon.ui").nav_next()<CR>]])
-map('n', '<A-1>', [[:lua require("harpoon.ui").nav_file(1)<CR>]])
-map('n', '<A-2>', [[:lua require("harpoon.ui").nav_next(2)<CR>]])
-map('n', '<A-3>', [[:lua require("harpoon.ui").nav_next(3)<CR>]])
-map('n', '<A-4>', [[:lua require("harpoon.ui").nav_next(4)<CR>]])
+map("n", "<A-p>", [[:lua require("harpoon.ui").toggle_quick_menu()<CR>]])
+map("n", "<leader>lf", [[:lua require("harpoon.mark").add_file()<CR>]])
+map("n", "<leader>l;", [[:lua require("harpoon.ui").nav_prev()<CR>]])
+map("n", "<leader>ll", [[:lua require("harpoon.ui").nav_next()<CR>]])
+map("n", "<A-1>", [[:lua require("harpoon.ui").nav_file(1)<CR>]])
+map("n", "<A-2>", [[:lua require("harpoon.ui").nav_next(2)<CR>]])
+map("n", "<A-3>", [[:lua require("harpoon.ui").nav_next(3)<CR>]])
+map("n", "<A-4>", [[:lua require("harpoon.ui").nav_next(4)<CR>]])
 
 -- Telescope
 map("n", "<leader>ff", [[:lua require'telescope.builtin'.find_files()<CR>]])
