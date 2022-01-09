83eaf62e957596f3624a8085de81e533379ca71b
bryant-the-coder <bryantfoo99@gmail.com>
2022-01-09 12:53:06 +0800

    format

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,9 +1,9 @@
 local status_ok, treesitter = pcall(require, "nvim-treesitter.configs")
 if not status_ok then
-return
+	return
 end
 
-local incremental_selection = require "nvim-treesitter.incremental_selection"
+local incremental_selection = require("nvim-treesitter.incremental_selection")
 vim.lsp.handlers['textDocument/publishDiagnostics'] = vim.lsp.with(
     vim.lsp.diagnostic.on_publish_diagnostics,
     {
@@ -16,69 +16,69 @@ vim.lsp.handlers['textDocument/publishDiagnostics'] = vim.lsp.with(
     }
 )
 
-require 'nvim-treesitter.install'.compilers = { "clang" }
-treesitter.setup {
-  ensure_installed = {"html", "javascript", "lua", "scss"},
-  sync_install = false,
-  incremental_selection = {
-    enable = true,
-    keymaps = {
-      init_selection = "gnn",
-      node_incremental = "grn",
-      scope_incremental = "grc",
-      node_decremental = "grm",
-    },
-  },
-  highlight = {
-    enable = true,
-    additional_vim_regex_highlighting = false,
-  },
-  rainbow = {
-      enable = true,
-      extended_mode = false,
-  },
-  autotag = {
-    enable = true,
-  },
-  autopairs = {
-      enable = true,
-  },
-  indent = {
-    enable = true
-  },
-  context_commentstring = {
-    enable = true,
-    enable_autocmd = false,
-  },
-  query_linter = {
-    enable = true,
-    use_virtual_text = true,
-    lint_events = {"BufWrite", "CursorHold"},
- },
-  textobjects = {
-			select = {
-				enable = true,
-				lookahead = true,
+require("nvim-treesitter.install").compilers = { "clang" }
+treesitter.setup({
+	ensure_installed = { "html", "javascript", "lua", "scss" },
+	sync_install = false,
+	incremental_selection = {
+		enable = true,
+		keymaps = {
+			init_selection = "gnn",
+			node_incremental = "grn",
+			scope_incremental = "grc",
+			node_decremental = "grm",
+		},
+	},
+	highlight = {
+		enable = true,
+		additional_vim_regex_highlighting = false,
+	},
+	rainbow = {
+		enable = true,
+		extended_mode = false,
+	},
+	autotag = {
+		enable = true,
+	},
+	autopairs = {
+		enable = true,
+	},
+	indent = {
+		enable = true,
+	},
+	context_commentstring = {
+		enable = true,
+		enable_autocmd = false,
+	},
+	query_linter = {
+		enable = true,
+		use_virtual_text = true,
+		lint_events = { "BufWrite", "CursorHold" },
+	},
+	textobjects = {
+		select = {
+			enable = true,
+			lookahead = true,
+		},
+		move = {
+			enable = true,
+			set_jumps = true,
+			goto_next_start = {
+				["]m"] = "@function.outer",
+				["]]"] = "@class.outer",
 			},
-			move = {
-				enable = true,
-				set_jumps = true,
-				goto_next_start = {
-					["]m"] = "@function.outer",
-					["]]"] = "@class.outer",
-				},
-				goto_next_end = {
-					["]M"] = "@function.outer",
-					["]["] = "@class.outer",
-				},
-				goto_previous_start = {
-					["[m"] = "@function.outer",
-					["[["] = "@class.outer",
-				},
-				goto_previous_end = {
-					["[M"] = "@function.outer",
-					["[]"] = "@class.outer",
-				},
+			goto_next_end = {
+				["]M"] = "@function.outer",
+				["]["] = "@class.outer",
+			},
+			goto_previous_start = {
+				["[m"] = "@function.outer",
+				["[["] = "@class.outer",
+			},
+			goto_previous_end = {
+				["[M"] = "@function.outer",
+				["[]"] = "@class.outer",
 			},
 		},
-}
+	},
+})
