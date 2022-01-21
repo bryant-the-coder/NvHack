f8e369a191b3e8aac7b4cf980621be7de5cfbf76
bryant-the-coder <bryantfoo99@gmail.com>
2022-01-21 18:44:15 +0800

    Modified

--- lua/plugins/staline.lua
+++ lua/plugins/staline.lua
@@ -3,60 +3,99 @@ if not present then
    return
 end
 
--- staline.setup {
--- 	sections = {
--- 		left = {
--- 			'▊', ' ', { 'Evil', ' ' }, ' ',         -- The mode and evil sign
--- 			'file_size', ' ',                        -- Filesize
--- 			{ 'StalineFile', 'file_name' }, ' '       -- Filename in different highlight
--- 		},
--- 		mid = { ' ', 'lsp_name' },                      -- "lsp_name" is still a little buggy
--- 		right = {
--- 			{ 'StalineEnc', vim.bo.fileencoding:upper() }, '  ',  -- Example for custom section
--- 			{ 'StalineEnc', 'cool_symbol' }, ' ',                 -- the cool_symbol for your OS
--- 			{ 'StalineGit', 'branch' }, ' ', '▊'                  -- Branch Name in different highlight
--- 		}
--- 	},
--- 	defaults = {
--- 		bg = "#202328",
--- 		branch_symbol = " "
--- 	},
--- 	mode_colors = {
--- 		n = "#38b1f0",
--- 		i = "#9ece6a",       -- etc mode
--- 	}
--- }
--- vim.cmd [[hi Evil        guifg=#f36365 guibg=#202328]]       -- Higlight for Evil symbol
--- vim.cmd [[hi StalineEnc  guifg=#7d9955 guibg=#202328]]       -- Encoding Highlight
--- vim.cmd [[hi StalineGit  guifg=#8583b3 guibg=#202328]]       -- Branch Name Highlight
--- vim.cmd [[hi StalineFile guifg=#c37cda guibg=#202328]]       -- File name Highlight
+local percentage = function()
+	local current_line = vim.fn.line(".")
+	local total_lines = vim.fn.line("$")
+	local chars = { "__", "▁▁", "▂▂", "▃▃", "▄▄", "▅▅", "▆▆", "▇▇", "██" }
+	local line_ratio = current_line / total_lines
+	local index = math.ceil(line_ratio * #chars)
+	return chars[index]
+end
 
--- function()
---     local current_line = vim.fn.line "."
---     local total_lines = vim.fn.line "$"
---     local chars = { "_", "▁", "▂", "▃", "▄", "▅", "▆", "▇", "█" }
---     local line_ratio = current_line / total_lines
---     local index = math.ceil(line_ratio * #chars)
---     return chars[index]
--- end, ' '
+local time = function ()
+  return os.date("%a │ %H:%M %x")
+end
+
+-- local branch = function ()
+--   return " "..( vim.b.gitsigns_head:upper() or "")
+-- end
+
+local my_colors = {
+  n = "#ec5f67",
+  i = "#98be65",
+  c = "#c678dd",
+  v = "#51afef",
+}
 
 staline.setup {
   sections ={
     left = {
-      '▌', 'mode', {'StalineBranch', 'branch'},' ',
+      'mode',
+      ' ',
+      {'StalineBranch', 'branch'},
+      -- branch,
     },
     mid = {
-      {'StalineName', 'file_name'}
+      {'StalineName', 'file_name'},
     },
     right = {
-      '-lsp'
+      '-lsp',
+      ' ',
+      time,
+      '  ',
+      percentage,
     }
   },
+
   defaults = {
-    -- LSP highlighting
-    true_colors = true
-  }
+    true_colors   = true,       -- LSP highlighing
+		font_active   = "none",
+		branch_symbol = " ",       -- Change the branch symbol
+    mod_symbol    = "  ",      -- Change the modified symbol
+  },
+
+  -- I changed the mode icons to text
+	mode_icons = {
+		['n']    = 'NORMAL',
+		['no']   = 'NORMAL',
+		['nov']  = 'NORMAL',
+		['noV']  = 'NORMAL',
+		['no'] = 'NORMAL',
+		['niI']  = 'NORMAL',
+		['niR']  = 'NORMAL',
+		['niV']  = 'NORMAL',
+
+		['i']    = 'INSERT',
+		['ic']   = 'INSERT',
+		['ix']   = 'INSERT',
+		['s']    = 'INSERT',
+		['S']    = 'INSERT',
+
+		['v']    = 'VISUAL',
+		['V']    = 'VISUAL',
+		['']   = 'VISUAL',
+		['r']    = 'REPLACE',
+		['r?']   = 'REPLACE',
+    ['R']    = 'REPLACE',
+		['c']    = 'COMMAND',
+		['t']    = 'TERMINAL',
+	},
+
+  mode_colors = my_colors,      -- Change mode colors
+
+	special_table = {
+		NvimTree        = { 'File Explorer', ' ' },
+		packer          = { 'Packer', ' ' },
+    TelescopePrompt = { 'Telescope', ' ' }
+	},
+
+	lsp_symbols = {
+		Error =" ",
+		Info  =" ",
+		Warn  =" ",
+		Hint  =" ",
+	},
 }
 
 vim.cmd [[hi StalineBranch guifg=#d6c91b]]
-vim.cmd [[hi StalineName guifg=#FFFFFF]]
+vim.cmd [[hi StalineName guifg=#c37cda]]
