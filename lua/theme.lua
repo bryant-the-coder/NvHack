b42a33cdf342b7dc83c162f80f1e85b36afffb6b
bryant-the-coder <bryantfoo99@gmail.com>
2022-01-29 19:50:26 +0800

    empty

--- lua/theme.lua
+++ lua/theme.lua
@@ -8,7 +8,6 @@ require("themer").setup({
 		functionBuiltIn = { style = "italic" },
 		comment = { style = "bold" },
 		constant = { style = "bold" },
-		number = { style = "italic" },
 		diagnostic = {
 			underline = {
 				error = { style = "underline" },
@@ -21,25 +20,3 @@ require("themer").setup({
 		},
 	},
 })
---------------------------------
---      Time Changing Func    --
---------------------------------
--- if time.hour <= 10 then
---   vim.g.gruvbox_hls_cursor='red'
---   vim.g.gruvbox_number_column = 'bg0'
---   vim.g.gruvbox_sign_column = 'bg0'
---   vim.g.gruvbox_underline = false
---   vim.g.gruvbox_undercurl = false
---   vim.cmd[[colorscheme gruvbox]]
--- elseif time.hour <= 18 then
---   vim.g.rose_pine_bold_vertical_split_line = true
---   vim.g.rose_pine_disable_italics = true
---   vim.cmd[[colorscheme rose-pine]]
--- else
---   vim.g.tokyonight_style = "night"
---   vim.g.tokyonight_italic_comments = false
---   vim.g.tokyonight_italic_keywords = false
---   -- vim.g.tokyonight_transparent = true
---   vim.cmd[[colorscheme tokyonight]]
--- local time = os.date("*t")
--- end
