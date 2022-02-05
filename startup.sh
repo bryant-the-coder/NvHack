a3c78dd9d10ccc15d8d72724a56f22bc8a648ed8
TarunDaCoder <tarunsnair09@gmail.com>
2022-02-05 18:04:32 +0530

    add a startup.sh file

---
+++ startup.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+get_time() {
+  tail -1 tmp | cut -d ' ' -f 1
+}
+
+pf() {
+  printf '%s : ' "$@"
+}
+
+echo "Warmup #1" ; nvim
+echo "Warmup #2" ; nvim
+
+pf "No config"
+nvim -nu NORC --startuptime tmp; get_time
+
+pf "With config"
+nvim --startuptime tmp; get_time
+
+pf "Fastest config"
+nvim -nu "${XDG_CONFIG_HOME:-$HOME/.config}/nvim/init.lua" --startuptime tmp; get_time
+
+pf "Opening init.lua"
+nvim "${XDG_CONFIG_HOME:-$HOME/.config}/nvim/init.lua" --startuptime tmp; get_time
+
+pf "Opening Python file"
+nvim tmp.py --startuptime tmp; get_time
+
+pf "Opening C File"
+nvim tmp.c --startuptime tmp; get_time
+
+pf "Opening Norg file"
+nvim tmp.norg --startuptime tmp; get_time
+
+rm tmp
