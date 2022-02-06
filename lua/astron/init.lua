6998cf0b804c40f4f86922b3024db03712849ebd
bryant-the-coder <bryantfoo99@gmail.com>
2022-02-06 22:07:47 +0800

    created this theme
    from astrovim

---
+++ lua/astron/init.lua
@@ -0,0 +1,32 @@
+vim.cmd "hi clear"
+if vim.fn.exists "syntax_on" then
+  vim.cmd "syntax reset"
+end
+vim.o.background = "dark"
+vim.o.termguicolors = true
+vim.g.colors_name = "astron"
+
+C = require "astron.colors"
+
+local util = require "astron.util"
+local base = require "astron.base"
+local treesitter = require "astron.treesitter"
+local lsp = require "astron.lsp"
+local others = require "astron.others"
+local cmp = require "astron.cmp"
+local markdown = require"astron.markdown"
+
+local astron = {
+  base,
+  cmp,
+  treesitter,
+  markdown,
+  lsp,
+  others,
+}
+
+for _, file in ipairs(astron) do
+  for group, colors in pairs(file) do
+    util.highlight(group, colors)
+  end
+end
---
+++ lua/astron/markdown.lua
@@ -0,0 +1,12 @@
+local markdown = {
+  markdownH1 ={ fg = C.red_1, style = "bold,italic" },
+  markdownH2 ={ fg = C.green, style = "bold" },
+  markdownH3 ={ fg = C.blue, style = "bold" },
+  markdownH4 ={ fg = C.orange, style = "bold" },
+  markdownH5 ={ fg = C.yellow_1, style = "bold" },
+  markdownH6 ={ fg = C.yellow_1, style = "bold" },
+}
+
+return markdown
+
+-- TODO: follow this https://github.com/tiagovla/tokyodark.nvim/blob/master/lua/tokyodark/highlights.lua
