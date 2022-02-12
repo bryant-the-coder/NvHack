74cc7de9502d03bebe183d6f4b30c627132f6420
bryant-the-coder <bryantfoo99@gmail.com>
2022-02-12 11:22:55 +0800

    chore: autoformat with stylua

--- lua/settings.lua
+++ lua/settings.lua
@@ -7,32 +7,32 @@ local o = vim.opt
 o.guicursor = "n-v-c-sm:block,ci-ve:ver25,r-cr-o:hor20,i:block-blinkwait700-blinkoff400-blinkon250-Cursor/lCursor"
 
 -- Mappings
-g.mapleader = ","                         -- Setting map leader
-o.mouse = "a"                             -- Enabling mouse mode in all modes
+g.mapleader = "," -- Setting map leader
+o.mouse = "a" -- Enabling mouse mode in all modes
 
 -- Basics
-o.number = true                           -- Shows numbers
-o.relativenumber = true                   -- Shows relative numbers
-o.showmatch = true                        -- Shows show match
-o.cursorline = false                      -- Shows cursorline
-o.scrolloff = 200                         -- Stop scrolling on the number set
+o.number = true -- Shows numbers
+o.relativenumber = true -- Shows relative numbers
+o.showmatch = true -- Shows show match
+o.cursorline = false -- Shows cursorline
+o.scrolloff = 200 -- Stop scrolling on the number set
 o.splitright = true
 o.splitbelow = true
-o.smartcase = true                        -- Don't ignore when uppercase search
-o.wrap = false                            -- Don't wrap
-o.clipboard = "unnamedplus"               -- Use system clipboard
+o.smartcase = true -- Don't ignore when uppercase search
+o.wrap = false -- Don't wrap
+o.clipboard = "unnamedplus" -- Use system clipboard
 o.completeopt = "menuone,noselect,menu"
 o.foldexpr = "nvim_treesitter#foldexpr()"
-o.fillchars = { eob = " " }               -- Fillchars
-o.lazyredraw = true                       -- Don't redraw screen while processing macros
+o.fillchars = { eob = " " } -- Fillchars
+o.lazyredraw = true -- Don't redraw screen while processing macros
 o.termguicolors = true
 
 -- CMD line
-o.cmdheight = 1                           -- CMD height
+o.cmdheight = 1 -- CMD height
 o.hidden = true
-o.history = 300                           -- Remember 300 lines of history
+o.history = 300 -- Remember 300 lines of history
 o.updatetime = 200
-o.smd = false                             -- Don't show mode in cmdline
+o.smd = false -- Don't show mode in cmdline
 
 -- Tabs / Indent
 o.expandtab = true
@@ -42,10 +42,10 @@ o.smartindent = true
 o.smarttab = true
 
 -- Backups / Undo
-o.backup = false                          -- Don't backup files
-o.writebackup = false                     -- Don't write backup
-o.undofile = false                        -- Don't write undofile
-o.swapfile = false                        -- Don't write swapfile
+o.backup = false -- Don't backup files
+o.writebackup = false -- Don't write backup
+o.undofile = false -- Don't write undofile
+o.swapfile = false -- Don't write swapfile
 
 -- Search
 o.hlsearch = true
