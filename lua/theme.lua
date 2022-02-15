d5f5ba2f0d4f0ed73b14d70c3e55d3f3ce9031dc
bryant-the-coder <bryantfoo99@gmail.com>
2022-02-16 07:38:06 +0800

    changed to rosepine for testing

--- lua/theme.lua
+++ lua/theme.lua
@@ -1,26 +1,26 @@
--- require("themer").setup({
--- 	colorscheme = "darknight",
--- 	transparent = false,
--- 	disable_telescope_themes = { "ayu", "rose_pine_dawn" },
--- 	term_colors = true,
--- 	dim_inactive = false,
--- 	styles = {
--- 		["function"] = { style = "italic" },
--- 		functionBuiltIn = { style = "italic" },
--- 		comment = { style = "bold" },
--- 		number = { style = "italic" },
--- 		constant = { style = "bold" },
--- 		diagnostic = {
--- 			underline = {
--- 				error = { style = "underline" },
--- 				warn = { style = "underline" },
--- 			},
--- 			virtual_text = {
--- 				error = { style = "italic" },
--- 				warn = { style = "italic" },
--- 			},
--- 		},
--- 	},
--- })
+require("themer").setup({
+	colorscheme = "rose_pine",
+	transparent = false,
+	disable_telescope_themes = { "ayu", "rose_pine_dawn" },
+	term_colors = true,
+	dim_inactive = false,
+	styles = {
+		["function"] = { style = "italic" },
+		functionBuiltIn = { style = "italic" },
+		comment = { style = "italic" },
+		number = { style = "italic" },
+		constant = { style = "bold" },
+		diagnostic = {
+			underline = {
+				error = { style = "underline" },
+				warn = { style = "underline" },
+			},
+			virtual_text = {
+				error = { style = "italic" },
+				warn = { style = "italic" },
+			},
+		},
+	},
+})
 
 -- testing
