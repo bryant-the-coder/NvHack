a440771c13838d92b0f95180fdd6868863ff23d7
bryant-the-coder <bryantfoo99@gmail.com>
2022-02-22 18:32:26 +0800

    testing some stuff

--- lua/plugins/dashboard.lua
+++ lua/plugins/dashboard.lua
@@ -2,7 +2,7 @@ local g = vim.g
 local fn = vim.fn
 
 local plugins_count = fn.len(fn.globpath("$HOME/AppData/Local/nvim-data/site/pack/packer/start/", "*", 0, 1))
--- local datetime = os.date("%d-%m-%Y")
+local datetime = os.date("%d-%m-%Y")
 
 g.dashboard_disable_statusline = 1
 g.dashboard_default_executive = "telescope"
@@ -37,8 +37,8 @@ g.dashboard_custom_section = {
 }
 
 g.dashboard_custom_footer = {
-	-- "┌─ Today is " .. datetime .. " ─┐",
-	-- "└─ " .. plugins_count .. " plugins in total ─┘"
-	" ",
-	"NvHack loaded " .. plugins_count .. " plugins successfully!",
+	"┌─ Today is " .. datetime .. " ─┐",
+	"└─ " .. plugins_count .. " plugins in total ─┘",
+	-- " ",
+	-- "NvHack loaded " .. plugins_count .. " plugins successfully!",
 }
