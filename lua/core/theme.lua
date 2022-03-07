7574bbfe49b98bde98db6113742eba26447ee578
bryant-the-coder <bryantfoo99@gmail.com>
2022-03-07 09:59:24 +0800

    theme

--- lua/core/theme.lua
+++ lua/core/theme.lua
@@ -1,27 +1,30 @@
--- require("themer").setup({
--- 	colorscheme = "gruvhad",
--- 	transparent = false,
--- 	disable_telescope_themes = { "ayu", "rose_pine_dawn" },
--- 	term_colors = true,
--- 	dim_inactive = false,
--- 	styles = {
--- 		["function"] = { style = "italic" },
--- 		functionBuiltIn = { style = "italic" },
--- 		comment = { style = "italic,bold" },
--- 		number = { style = "italic" },
--- 		constant = { style = "bold" },
--- 		diagnostic = {
--- 			underline = {
--- 				error = { style = "underline" },
--- 				warn = { style = "underline" },
--- 			},
--- 			virtual_text = {
--- 				error = { style = "italic" },
--- 				warn = { style = "italic" },
--- 			},
--- 		},
--- 	},
--- })
+require("themer").setup({
+	colorscheme = "gruvchad",
+	transparent = false,
+	disable_telescope_themes = { "ayu", "rose_pine_dawn" },
+	term_colors = true,
+	dim_inactive = false,
+	styles = {
+		["function"] = { italic = true },
+		functionBuiltIn = { italic = true },
+		comment = { italic = true, bold = true },
+		number = { italic = true },
+		constant = { bold = true },
+		diagnostic = {
+			underline = {
+				error = { underline = true },
+				warn = { underline = true },
+			},
+			virtual_text = {
+				error = { italic = true },
+				warn = { italic = true },
+			},
+		},
+	},
+})
 
-local base16 = require("base16")
-base16(base16.themes("gruvchad"))
+-- local base16 = require("base16")
+-- base16(base16.themes("gruvchad"))
+
+-- vim.g.astron_italic_comments = false
+-- vim.cmd([[colorscheme astron]])
