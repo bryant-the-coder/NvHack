64ef703d6452c702ecbc591c841fef5b0c25a800
bryant-the-coder <bryantfoo99@gmail.com>
2022-03-15 23:04:38 +0800

    code from TJ

--- lua/plugins/config/lsp/handlers.lua
+++ lua/plugins/config/lsp/handlers.lua
@@ -69,6 +69,14 @@ M.setup = function()
 				end
 				return i .. "/" .. total .. " " .. icon .. "  ", highlight
 			end,
+			-- format = function(d)
+			-- 	local t = vim.deepcopy(d)
+			-- 	local code = d.code or d.user_data.lsp.code
+			-- 	if code then
+			-- 		t.message = string.format("%s [%s]", t.message, code):gsub("1. ", "")
+			-- 	end
+			-- 	return t.message
+			-- end,
 		},
 	}
 
