dd932027290cb65ecffb691685c7aa5ed6d78fdc
bryant-the-coder <bryantfoo99@gmail.com>
2022-03-17 12:13:41 +0800

    thanks TJ

--- lua/plugins/config/snippets.lua
+++ lua/plugins/config/snippets.lua
@@ -1,6 +1,30 @@
 local ls = require("luasnip")
 local parse = ls.parser.parse_snippet
+local types = require("luasnip.util.types")
 require("luasnip.loaders.from_vscode").lazy_load()
+
+ls.config.set_config({
+	-- This tells LuaSnip to remember to keep around the last snippet.
+	-- You can jump back into it even if you move outside of the selection
+	history = true,
+
+	-- This one is cool cause if you have dynamic snippets, it updates as you type!
+	updateevents = "TextChanged,TextChangedI",
+
+	-- Autosnippets:
+	enable_autosnippets = true,
+
+	-- Crazy highlights!!
+	-- #vid3
+	-- ext_opts = nil,
+	ext_opts = {
+		[types.choiceNode] = {
+			active = {
+				virt_text = { { "<-", "Error" } },
+			},
+		},
+	},
+})
 
 local module = [[
 local M = {}
