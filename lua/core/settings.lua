ee016a7acdc621cca56f95a57c83aa30c0fc7a95
bryant-the-coder <bryantfoo99@gmail.com>
2022-03-23 21:47:28 +0800

    conceal level set

--- lua/core/settings.lua
+++ lua/core/settings.lua
@@ -2,6 +2,7 @@
 local g = vim.g
 local o = vim.opt
 
+o.conceallevel = 2
 -- Non-blinking in normal & visual mode
 -- Blink in insert mode
 o.guicursor = "n-v-c-sm:block,ci-ve:ver25,r-cr-o:hor20,i:block-blinkwait700-blinkoff400-blinkon250-Cursor/lCursor"
