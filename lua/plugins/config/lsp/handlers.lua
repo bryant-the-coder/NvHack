2288108daead9ef95a7940ea614f9d90ee1cafd0
bryant-the-coder <bryantfoo99@gmail.com>
2022-04-01 18:59:36 +0800

    switch places for lsp

--- lua/plugins/config/lsp/handlers.lua
+++ lua/plugins/config/lsp/handlers.lua
@@ -29,8 +29,8 @@ M.setup = function()
 	local signs = {
 		{ name = "DiagnosticSignError", text = "" },
 		{ name = "DiagnosticSignWarn", text = "" },
-		{ name = "DiagnosticSignHint", text = "" },
 		{ name = "DiagnosticSignInfo", text = "" },
+		{ name = "DiagnosticSignHint", text = "" },
 	}
 	for _, sign in ipairs(signs) do
 		vim.fn.sign_define(sign.name, { texthl = sign.name, text = sign.text, numhl = "" })
