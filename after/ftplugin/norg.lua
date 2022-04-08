579450e2d2656683e55d4fc9f1cda684e38797ae
bryant-the-coder <bryantfoo99@gmail.com>
2022-04-09 07:07:49 +0800

    feat(ftplugin): Added neorg

---
+++ after/ftplugin/norg.lua
@@ -0,0 +1,7 @@
+vim.bo.shiftwidth = 2
+vim.o.conceallevel = 2
+vim.bo.commentstring = "#%s"
+vim.wo.spell = true
+vim.bo.spelllang = "en"
+vim.o.foldenable = false
+vim.cmd([[hi link NeorgMarkupVerbatim Comment]])
