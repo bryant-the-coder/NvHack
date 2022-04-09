6f85a6df1ed7015bf44da22087729e7352f35e0d
bryant-the-coder <bryantfoo99@gmail.com>
2022-04-09 19:14:58 +0800

    feat(ui): Bufferline fill is slowing the editor down

--- lua/plugins/config/bufferline.lua
+++ lua/plugins/config/bufferline.lua
@@ -38,6 +38,23 @@ bufferline.setup({
         show_tab_indicators = true,
         enforce_regular_tabs = true,
         always_show_bufferline = true,
+        custom_filter = function(buf_number)
+            -- Func to filter out our managed/persistent split terms
+            local present_type, type = pcall(function()
+                return vim.api.nvim_buf_get_var(buf_number, "term_type")
+            end)
+
+            if present_type then
+                if type == "vert" then
+                    return false
+                elseif type == "hori" then
+                    return false
+                end
+                return true
+            end
+
+            return true
+        end,
     },
     highlights = {
         background = {
@@ -91,10 +108,10 @@ bufferline.setup({
             guifg = default.colors.red,
             guibg = default.colors.black,
         },
-        fill = {
-            guifg = default.colors.grey_fg,
-            guibg = "NONE",
-        },
+        -- fill = {
+        --     guifg = default.colors.grey_fg,
+        --     guibg = "NONE",
+        -- },
         indicator_selected = {
             guifg = default.colors.black,
             guibg = default.colors.black,
