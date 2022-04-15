ae5152827438eb7210f3fbb43530a0b032a94749
bryant-the-coder <bryantfoo99@gmail.com>
2022-04-15 21:18:27 +0800

    fix(null): Comment out prettier as it is slow

--- lua/plugins/config/null-ls.lua
+++ lua/plugins/config/null-ls.lua
@@ -7,9 +7,9 @@ local formatting = null_ls.builtins.formatting
 null_ls.setup({
     debug = false,
     sources = {
-        formatting.prettier.with({
-            extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" },
-        }),
+        -- formatting.prettier.with({
+        --     extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" },
+        -- }),
         formatting.stylua,
         formatting.rustfmt,
         formatting.clang_format,
