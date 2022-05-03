162a9400fcb027a9046f4a0107cec75891c111d3
bryant-the-coder <bryantfoo99@gmail.com>
2022-05-03 10:26:24 +0800

    fix(mapping): Change mapping for neorg

--- lua/plugins/config/neorg.lua
+++ lua/plugins/config/neorg.lua
@@ -116,7 +116,7 @@ neorg_callbacks.on_event("core.keybinds.events.enable_keybinds", function(_, key
     -- Map all the below keybinds only when the "norg" mode is active
     keybinds.map_event_to_mode("norg", {
         n = { -- Bind keys in normal mode
-            { "<C-s>", "core.integrations.telescope.find_linkable" },
+            { "<C-f>", "core.integrations.telescope.find_linkable" },
         },
 
         i = { -- Bind in insert mode
