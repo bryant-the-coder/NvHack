166943dbaf52a9426fa0d79d46582ebb00143415
bryant-the-coder <bryantfoo99@gmail.com>
2022-05-08 21:04:27 +0800

    fix(settings): Organize settings.lua

--- lua/core/settings.lua
+++ lua/core/settings.lua
@@ -1,48 +1,48 @@
--- Aliases for Neovim
 local g = vim.g
 local o = vim.opt
 
--- Non-blinking in normal & visual mode
--- Blink in insert mode
+--Cursor & cursorline
 o.guicursor = "n-v-c-sm:block,ci-ve:ver25,r-cr-o:hor20,i:block-blinkwait700-blinkoff400-blinkon250-Cursor/lCursor"
-
--- Cursorline
--- See :h 'cursorlineopt'
 o.cursorline = true
 o.cursorlineopt = { "number" } -- Highlighting the number where the cursor is on
 
 -- Mappings
-g.mapleader = "," -- Setting map leader
-o.mouse = "a" -- Enabling mouse mode in all modes
+g.mapleader = ","
+o.mouse = "a"
 
--- Theme
-o.termguicolors = true
-
--- Basics
-o.number = true -- Shows numbers
-o.relativenumber = true -- Shows relative numbers
+-- Number
+o.number = true
+o.relativenumber = true
 o.signcolumn = "yes:2"
-o.scrolloff = 200 -- Stop scrolling on the number set
 
 -- Splits
 o.splitright = true
 o.splitbelow = true
 
--- Editor
-o.wrap = false -- Don't wrap
-o.clipboard = "unnamedplus" -- Use system clipboard
-o.foldmethod = "marker"
-o.completeopt = "menuone,noselect,menu"
-o.lazyredraw = true -- Don't redraw screen while processing macros
-o.confirm = true
-
 -- Command line
-o.shortmess:append("I") -- Less and shorter messages in command line
-o.cmdheight = 1 -- CMD height
+o.shortmess:append("I")
+o.cmdheight = 1
 o.hidden = true
-o.history = 300 -- Remember 300 lines of history
+o.history = 300
 o.updatetime = 200
 o.smd = false -- Don't show mode in cmdline
+
+-- Tabs & Indent
+o.expandtab = true
+o.tabstop = 4
+o.shiftwidth = 4
+o.smartindent = true
+o.autoindent = true
+o.smarttab = true
+o.linebreak = true
+o.breakindent = true
+
+-- Search
+o.hlsearch = true
+o.incsearch = true
+o.ignorecase = true
+o.showmatch = true -- Shows show match
+o.smartcase = true -- Don't ignore when uppercase search
 
 -- Statusline
 o.laststatus = 3
@@ -81,34 +81,13 @@ end
 
 window_separator(true)
 
--- Tabs / Indent
-o.expandtab = true
-o.tabstop = 4
-o.shiftwidth = 4
-o.smartindent = true
-o.autoindent = true
-o.smarttab = true
-o.linebreak = true
-o.breakindent = true
-
 -- Backups / Undo
 o.backup = false -- Don't backup files
 o.writebackup = false -- Don't write backup
 o.undofile = false -- Don't write undofile
 o.swapfile = false -- Don't write swapfile
 
--- Search
-o.hlsearch = true
-o.incsearch = true
-o.ignorecase = true
-o.showmatch = true -- Shows show match
-o.smartcase = true -- Don't ignore when uppercase search
-
--- Python
-g.python3_host_prog = "C:/Python310/python.exe"
-
--- Add if else statements so linux/mac users can use
-
+-- Terminal
 if vim.fn.has("windows") == 1 then
     o.shell = "C:/Windows/System32/WindowsPowerShell/v1.0/powershell.exe"
 end
